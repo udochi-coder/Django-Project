624b3eb3a192d6e4109fb36077568df775acc1e9
udochi-coder <ashiegbuudochi@gmail.com>
2026-09-18 06:28:37 +0100

    Fix ALLOWED_HOSTS to wildcard .vercel.app domainssss urls

--- ResultPortal/urls.py
+++ ResultPortal/urls.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
-from django.urls import path,include
-
-from django.urls import re_path
+from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
 from django.http import JsonResponse
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 def root_view(request):
     return JsonResponse({
@@ -14,6 +26,7 @@ def root_view(request):
         "message": "ResultPortal API",
         "docs": "/swagger/"
     })
+
 
 urlpatterns = [
     path('', root_view, name='root'),
