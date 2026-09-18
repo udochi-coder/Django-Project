7a2f9f284e6780c13e297490a0bc3c0b3813af4f
udochi-coder <ashiegbuudochi@gmail.com>
2026-09-18 06:08:24 +0100

    Fix ALLOWED_HOSTS to wildcard .vercel.app domains

--- ResultPortal/settings.py
+++ ResultPortal/settings.py
@@ -50,11 +50,10 @@ ALLOWED_HOSTS = [
     host.strip()
     for host in os.getenv(
         'ALLOWED_HOSTS',
-        'localhost,127.0.0.1,0.0.0.0,[::1],web,northstar-university.vercel.app,https://django-project-hc1a.onrender.com'
+        'localhost,127.0.0.1,0.0.0.0,[::1],.vercel.app,django-project-hc1a.onrender.com'
     ).split(',')
     if host.strip()
 ]
-
 # Application definition
 
 INSTALLED_APPS = [
